e95eda5bc56e9c0c4c2a1c0d19703b4b647bc6ba
Didar Abdulkhaliq <me@didar.dev>
2025-09-15 16:26:02 +0300

    Refactor Navbar component and implement Categories fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cookies } from "next/headers";
-import Navbar from "../../components/Navbar";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
 
 const FetchProfile = async () => {
   const accessToken = (await cookies()).get("accessToken")?.value;
-
   if (!accessToken) {
     return null;
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
-
+import Categories from "@/components/Home/Categories";
+import { Suspense } from "react";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <div>
+      <Suspense fallback={<div>Loading categories...</div>}>
+        <Categories />
+      </Suspense>
+    </div>
   );
 }
